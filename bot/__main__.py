76d95641dc174a48968a0ebcbb7824cc520c6118
Raul <raullarosa.personal@gmail.com>
2017-05-25 09:27:07 -0400

    adding context

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,19 +1,27 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
+context = [] # passed to ML Engine
 bot = ChatBot("Terminal",
     storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
     logic_adapter=[
         'chatterbot.logic.SpecificResponseAdapter',
         'chatterbot.logic.LowConfidenceAdapter',
+        #ML Adapter
     ],
     input_adapter="chatterbot.input.TerminalAdapter",
-    output_adapter="chatterbot.output.TerminalAdapter",
-    database="../database.db"
+    output_adapter="chatterbot.output.TerminalAdapter", #
+    database="../database.db",
+    read_only=True
 )
 
-bot.storage.drop()
+# bot.storage.drop()
 bot.set_trainer(ListTrainer)
+
+bot.train([
+    "Hi",
+    "Hello there. What can I help you with today?",
+])
 
 bot.train([
     "Ansr",
@@ -21,8 +29,8 @@ bot.train([
 ])
 
 bot.train([
-    "Hi",
-    "Hello there. What can I help you with today?",
+    "Who are you?",
+    "I'm Ansr, the Automatic Neural System Reponse. What can I help you with?",
 ])
 
 bot.train([
@@ -61,8 +69,8 @@ bot.train([
 ])
 
 bot.train([
-    "Who are you?",
-    "I'm Ansr, the Automatic Neural System Reponse. What can I help you with?",
+    "I have a question about",
+    "Hit me",
 ])
 
 print("Hi I'm Ansr. I'm the ultimate resource for ulti handbook questions. What can I help you with today?")
@@ -73,6 +81,14 @@ while True:
         # We pass None to this method because the parameter
         # is not used by the TerminalAdapter
         bot_input = bot.get_response(None)
+        if (bot.followup_question(bot_input)):
+            # append input to valid info array
+            print(bot.conversation)
+            context.append(bot.fixed_queue.peek().text)
+        else:
+            # empty out valid info array
+            context = []
+        print(context)
 
     # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit):
